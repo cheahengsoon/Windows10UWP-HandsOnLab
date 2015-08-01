116c5bb74535e8643a266a281dae75ab04109d26
Tom Walker <tomwalkercodes@outlook.com>
2015-08-01 16:18:26 -0400

    Checking in Lab1
    Checking code and docs for Lab1

---
+++ Lab1/Completed/HowToBBQ.Win10/HowToBBQ.Win10/ViewModels/MenuItem.cs
@@ -0,0 +1,43 @@
+﻿using Mvvm;
+using System;
+using System.Windows.Input;
+
+namespace HowToBBQ.Win10.ViewModels
+{
+    public class MenuItem : BindableBase
+    {
+        private string glyph;
+        private string text;
+        private DelegateCommand command;
+        private Type navigationDestination;
+
+        public string Glyph
+        {
+            get { return glyph; }
+            set { SetProperty(ref glyph, value); }
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set { SetProperty(ref text, value); }
+        }
+
+        public ICommand Command
+        {
+            get { return command; }
+            set { SetProperty(ref command, (DelegateCommand)value); }
+        }
+
+        public Type NavigationDestination
+        {
+            get { return navigationDestination; }
+            set { SetProperty(ref navigationDestination, value); }
+        }
+
+        public bool IsNavigation
+        {
+            get { return navigationDestination != null; }
+        }
+    }
+}
